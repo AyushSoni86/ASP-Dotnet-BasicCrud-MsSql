5b4194502e3b297b389eab50e1d955bdfc5b4388
Ayush Soni <ayush.soni2464@gmail.com>
2023-12-20 16:37:15 +0530

    Add project files.

---
+++ BasicCrud-MsSql/Models/BrandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BasicCrud_MsSql.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
